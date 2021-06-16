e76da6d93fa30ab50591aeb82c5eef7b4b67210a
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2021-06-16 21:42:15 +0300

    Mvc Proje Kamp
    Murat Yücedağ Mvc Proje Kampi

---
+++ MVCProjeKampi/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+      
+    }
+}
---
+++ MVCProjeKampi/DataAccessLayer/Repositories/WriterRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using EntityLayer;
+
+namespace DataAccessLayer.Repositories
+{
+    public class WriterRepository : IWriterDal
+    {
+        public void Insert(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List(Expression<Func<Writer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
